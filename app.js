0d4b9054cf67c9ba279bddb912605767bc6064bd
dkhairat <196094924+dkhairat@users.noreply.github.com>
2025-10-02 22:29:31 +0000

    mobile strategy changed to still scrubbing to avoid safari block of autoplay

--- app.js
+++ app.js
@@ -1,19 +1,46 @@
 const isTouchDevice = () => "ontouchstart" in window || navigator.maxTouchPoints > 0;
 const isMobileOrTablet = () => /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
-const VIDEO_DURATION = 3;
-const VIDEO_FWD_PATH = "assets/cover-f.mp4";
-const VIDEO_FWD_REV_PATH = "assets/cover-fr.mp4";
+const getImgPath = (i) => `assets/stills/frame_${String(i).padStart(3, "0")}.webp`;
 
-const video = document.querySelector("#image");
+const NUM_STILLS = 63;
+const FRAME_RATE = 20;
+const MS = 1_000;
+const VIDEO_PATH = "assets/cover-f.mp4";
+const VIDEO_DURATION = 3;
+
+const video = document.querySelector("video");
+const img = document.querySelector("img");
 
 if (isTouchDevice() || isMobileOrTablet()) {
-  video.src = VIDEO_FWD_REV_PATH;
-  video.loop = true;
-  video.play();
+  var index = 0;
+  var fwd = true;
+  img.src = getImgPath(index);
+
+  const stepFrame = () => {
+    if (fwd) {
+      index++;
+      if (index >= NUM_STILLS) {
+        index = NUM_STILLS;
+        fwd = false;
+      }
+    } else {
+      index--;
+      if (index <= 0) {
+        index = 0;
+        fwd = true;
+      }
+    }
+
+    img.src = getImgPath(index);
+  };
+
+  setInterval(stepFrame, MS / FRAME_RATE);
+  video.style.display = "none";
 } else {
   const body = document.querySelector("body");
   video.pause();
-  video.src = VIDEO_FWD_PATH;
+  video.src = VIDEO_PATH;
 
   body.addEventListener("mousemove", (e) => (video.currentTime = (e.clientX / body.clientWidth) * VIDEO_DURATION));
+  image.style.display = "none";
 }
