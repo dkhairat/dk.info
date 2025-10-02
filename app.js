272b5db3f59018e6fe24505e4f8608c482adf8c8
dkhairat <196094924+dkhairat@users.noreply.github.com>
2025-10-02 21:43:00 +0000

    continuing bug fixes

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const isTouchDevice = () => "ontouchstart" in window || navigator.maxTouchPoints > 0;
-const isMobileOrTablet = () => /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
-const VIDEO_DURATION = 3;
-const VIDEO_FWD_PATH = "assets/cover-f.mp4";
-const VIDEO_FWD_REV_PATH = "assets/cover-fr.mp4";
+// const isTouchDevice = () => "ontouchstart" in window || navigator.maxTouchPoints > 0;
+// const isMobileOrTablet = () => /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
+// const VIDEO_DURATION = 3;
+// const VIDEO_FWD_PATH = "assets/cover-f.mp4";
+// const VIDEO_FWD_REV_PATH = "assets/cover-fr.mp4";
 
-const video = document.querySelector("#image");
+// const video = document.querySelector("#image");
 
-if (isTouchDevice() || isMobileOrTablet()) {
-  video.src = VIDEO_FWD_PATH;
-  video.loop = true;
-  video.play();
-} else {
-  const body = document.querySelector("body");
-  video.pause();
-  video.src = VIDEO_FWD_PATH;
+// if (isTouchDevice() || isMobileOrTablet()) {
+//   video.src = VIDEO_FWD_REV_PATH;
+//   video.loop = true;
+//   video.play();
+// } else {
+//   const body = document.querySelector("body");
+//   video.pause();
+//   video.src = VIDEO_FWD_PATH;
 
-  body.addEventListener("mousemove", (e) => (video.currentTime = (e.clientX / body.clientWidth) * VIDEO_DURATION));
-}
+//   body.addEventListener("mousemove", (e) => (video.currentTime = (e.clientX / body.clientWidth) * VIDEO_DURATION));
+// }
